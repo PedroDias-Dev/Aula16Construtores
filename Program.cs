e2c2367730a2415ff8d1caf925cd459d67aa1293
Pedro Dias <pedrofelipe18@hotmail.com.br>
2020-06-12 14:29:29 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Aula16Construtores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // sem argumento
+            Produto sapato = new Produto();
+            sapato.Nome = "Vizzano";
+
+            //1 argumento
+            Produto chinelo = new Produto(45);
+            Console.WriteLine(chinelo.Codigo);
+
+            // todos os argumentos
+            Produto camisa = new Produto(34, "Bape", "Roxa Camo", 984);
+            Console.WriteLine(camisa.Codigo);
+            Console.WriteLine(camisa.Estoque);
+            Console.WriteLine(camisa.Descricao);
+            Console.WriteLine(camisa.Nome);
+
+        }
+    }
+}
